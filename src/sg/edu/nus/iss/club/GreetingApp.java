9898aa05c2301c92985ff8a88a2440617376c408
unknown <A0135945X@CR3547.iss.nus.edu.sg>
2015-02-28 15:10:37 +0800

    no message

--- src/sg/edu/nus/iss/club/GreetingApp.java
+++ src/sg/edu/nus/iss/club/GreetingApp.java
@@ -1,10 +1,22 @@
 package sg.edu.nus.iss.club;
 
+import java.time.LocalDateTime;
+
 public class GreetingApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Good Day!!!");
+		
+		
+		//Adding greeting based on the time of the day 
+		
+		int hour = LocalDateTime.now().getHour();
+		if (hour <12){
+			System.out.println("Good Morning!!!");
+		}else if(hour < 16){
+			System.out.println("Good Afternon!!!");
+		}else {
+			System.out.println("Good Night!!!");
+		}
 	}
-
 }
