f760be9aba982a168e480e463b87e8451f274a49
Su <A0006524@u.nus.edu>
2015-02-28 15:06:58 +0800

    different  way to printing current time

--- src/sg/edu/nus/iss/club/GreetingApp.java
+++ src/sg/edu/nus/iss/club/GreetingApp.java
@@ -1,10 +1,28 @@
 package sg.edu.nus.iss.club;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
 public class GreetingApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Good Day!!!");
+		
+		//Su added.. print out current date time.
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		System.out.println("Date Format"+dateFormat.format(date)); //2014/08/06 15:59:48		
+		System.out.println("LocalDateTime.Now()"+LocalDateTime.now());
+		System.out.println("LocalDate.Now()"+LocalDate.now());
+		System.out.println("ZonedDateTime.Now()"+ZonedDateTime.now());
+		
+		
+		
 	}
 
 }
