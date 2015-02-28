da829e108cc9802811281b8466d3d51e2ba24d1f
Su <A0006524@u.nus.edu>
2015-02-28 14:56:21 +0800

    Version 1.0 Good Day

---
+++ src/sg/edu/nus/iss/club/GreetingApp.java
@@ -0,0 +1,10 @@
+package sg.edu.nus.iss.club;
+
+public class GreetingApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Good Day!!!");
+	}
+
+}
