933fc98e12fa2cccda47978004478d46dad6079b
Su <A0006524@u.nus.edu>
2015-05-13 19:39:05 +0800

    check in

--- src/sg/edu/nus/iss/club/su/Person.java
+++ src/sg/edu/nus/iss/club/su/Person.java
@@ -1,5 +1,40 @@
 package sg.edu.nus.iss.club.su;
 
 public class Person {
+	
+	private String surname;
+	private String firstname;
+	private String secondname;
+	
+	public Person(String surname,String firstname,String secondname)
+	{
+		this.surname=surname;
+		this.firstname=firstname;
+		this.secondname=secondname;
+		
+	}
+	public void show()
+	{
+		String fullname=getSurname()+getFirstname()+getSecondname();
+		System.out.println(fullname);
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getSecondname() {
+		return secondname;
+	}
+	public void setSecondname(String secondname) {
+		this.secondname = secondname;
+	}
 
 }
