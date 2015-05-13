697192574089e40a2fbc2ef900ba1df681d8c6c9
Oscar <oscar.castro@bcdtravelapac.com>
2015-05-13 19:55:44 +0800

    adding junit tests

---
+++ test/sg/edu/nus/iss/club/oscar/MemberTest.java
@@ -0,0 +1,14 @@
+package sg.edu.nus.iss.club.oscar;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MemberTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
